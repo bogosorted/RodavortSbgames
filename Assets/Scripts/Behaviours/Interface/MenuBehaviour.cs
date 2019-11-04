af622684da58cf70f73ccc1ef0645114d395264f
Gabriel Aragao <gabrielaragaofds@gmail.com>
2019-11-03 22:33:56 -0300

    vs 0.0.1.0 base and menu implemented

---
+++ Assets/Scripts/Behaviours/Interface/MenuBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI UI = GetComponent<UI>();
+        UI.Fades(true ,3f, Random.Range(0,3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CenarioProcedural.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CenarioProcedural : MonoBehaviour
+{
+    [SerializeField] GameObject[] limitandos = new GameObject[4];
+    void Start()
+    {
+      //  InvokeRepeating("Layer3", 3, 6);
+        InvokeRepeating("Layer2", 3, 133);
+        InvokeRepeating("Layer1", 2, 13);
+
+    }
+
+
+    void Layer1() {
+        Instantiate(limitandos[0], new Vector3(gameObject.transform.position.x + 1100, gameObject.transform.position.y , 0), Quaternion.identity).transform.SetParent(gameObject.transform, false);
+    }
+
+    void Layer2() => Instantiate(limitandos[1], new Vector3(gameObject.transform.position.x + 1100, gameObject.transform.position.y , -100), Quaternion.identity).transform.SetParent(gameObject.transform, false);
+
+   //void Layer3() => Instantiate(limitandos[2]).transform.SetParent(gameObject.transform, false);
+    
+}
