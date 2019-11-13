735deb64755f90cd3f3ef70b26be988ce84ca214
Aluno <gabrielaragaofds@gmail.com>
2019-11-13 12:02:47 -0300

    0.0.2.2 Ajustes na angulação da carta

--- Assets/Scripts/Behaviours/Objects/Deck.cs
+++ Assets/Scripts/Behaviours/Objects/Deck.cs
@@ -45,18 +45,27 @@ public class Deck : MonoBehaviour
     {
         float angulacaoConst = mao.Count % 2 == 0f ? zMax / (float)(mao.Count / 2) : zMax / (float)((mao.Count - 1) / 2);
         float concatenador = -zMax;
-    
+        
         foreach (var x in mao)
         {
-            x.GetComponent<Image>().rectTransform.anchoredPosition = new Vector3(concatenador * mao.Count * 3f,-320);//(-Mathf.Abs(concatenador)*3)- 320 )
+            x.GetComponent<Image>().rectTransform.anchoredPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador)*3 - 300);
             if (mao.Count == 1)
-                concatenador = 0;
+                concatenador = 0;        
             //angulação Z retirada
-            //x.transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,-concatenador);
+            x.transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,-concatenador);
+            if (concatenador == 0  || concatenador == zMax || concatenador == -zMax)
+            {
+                x.GetComponent<Image>().rectTransform.anchoredPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador) * 3 - 306);
+            }
             concatenador += angulacaoConst;
             if (mao.Count % 2 == 0 && concatenador == 0)
+            {
+                      
                 concatenador += angulacaoConst;
-                
+            }
+            x.transform.SetSiblingIndex(-1);
+
+
         }
     }
     public void DesfazerSilhuetas()
