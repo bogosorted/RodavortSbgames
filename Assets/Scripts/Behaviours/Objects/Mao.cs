0991c5e8d09ef15142b3a6e53e2b8d731aa17ce5
Aluno <gabrielaragaofds@gmail.com>
2020-02-10 13:25:01 -0300

    programa de ajuste da angulação alterado

--- Assets/Scripts/Behaviours/Objects/Mao.cs
+++ Assets/Scripts/Behaviours/Objects/Mao.cs
@@ -8,6 +8,8 @@ public class Mao : MonoBehaviour
     public List<GameObject> mao = new List<GameObject>();
     [SerializeField] private GameObject carta;
     [SerializeField] Sprite[] ImagemCarta;
+    float step;
+    float x;
     
     
     public void SetCarta(int id)
@@ -17,51 +19,62 @@ public class Mao : MonoBehaviour
      objCarta.transform.SetParent(transform,false);
      mao.Add(objCarta);
     }
-    /*     public void SetAnguloTeste(float x,float step)
+
+/*
+         public void SetAnguloTeste(float max)
     {
-        step = 0f; x = 0f;
-        float angulacaoConst = mao.Count % 2 == 0f ? 12 / (float)(mao.Count / 2) : 12 / (float)((mao.Count - 1) / 2);
-        float concatenador = -12;
+        float angulacaoConst = mao.Count % 2 == 0f ? max / (float)(mao.Count / 2) : max / (float)((mao.Count - 1) / 2);
+        float concatenador = -max;
          if (mao.Count == 1)
-             concatenador = 0;  
-        
-       
+             concatenador = 0;            
          foreach(var y in mao)
         {                      
             y.GetComponent<Carta>().AngulacaoInicial = y.transform.position;         
             y.GetComponent<Carta>().AngulacaoFinal = new Vector2(concatenador,y.transform.position.y);
             concatenador += angulacaoConst;
-            y.transform.SetSiblingIndex(-1);
+            y.transform.SetSiblingIndex(3);
         }
+        InvokeRepeating("Angular",0,0);
     }
-    */
-
-    public void SetAnguloZ(int zMax)
+    private void Angular() 
     {
+        step = (Mathf.Pow(-x,2) + 2*x);       
+        if (step>=1)
+            CancelInvoke("Angular");
+        x += 0.2f * Time.deltaTime;
+        foreach(var p in mao)
+        {
+            p.transform.position = Vector2.Lerp(p.GetComponent<Carta>().AngulacaoInicial, p.GetComponent<Carta>().AngulacaoFinal, step);
+        }
+
+    }
+*/
+    //public void SetAnguloZ(int zMax)
+    //{
              
     
-        float angulacaoConst = mao.Count % 2 == 0f ? zMax / (float)(mao.Count / 2) : zMax / (float)((mao.Count - 1) / 2);
-        float concatenador = -zMax;
-        foreach (var x in mao)
-        {
+    //    float angulacaoConst = mao.Count % 2 == 0f ? zMax / (float)(mao.Count / 2) : zMax / (float)((mao.Count - 1) / 2);
+    //    float concatenador = -zMax;
+    //    foreach (var x in mao)
+    //    {
             
-            x.transform.localPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador)*3 - 304);
-            if (mao.Count == 1)
-                concatenador = 0;        
-            x.transform.GetChild(0).eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,-concatenador);
-            if (concatenador == 0  || concatenador == zMax || concatenador == -zMax)
-            {
-                x.transform.localPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador) * 3 - 310);
-            }
-            concatenador += angulacaoConst;
-            if (mao.Count % 2 == 0 && concatenador == 0)
-            {
+    //        x.transform.localPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador)*3 - 304);
+    //        if (mao.Count == 1)
+    //            concatenador = 0;        
+    //        x.transform.GetChild(0).eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,-concatenador);
+    //        if (concatenador == 0  || concatenador == zMax || concatenador == -zMax)
+    //        {
+    //            x.transform.localPosition = new Vector3(concatenador * mao.Count * 3f, -Mathf.Abs(concatenador) * 3 - 310);
+    //        }
+    //        concatenador += angulacaoConst;
+    //        if (mao.Count % 2 == 0 && concatenador == 0)
+    //        {
                       
-                concatenador += angulacaoConst;
-            }
-            x.transform.SetSiblingIndex(-1);
-        }
-    }
+    //            concatenador += angulacaoConst;
+    //        }
+    //        x.transform.SetSiblingIndex(-1);
+    //    }
+    //}
     public void AdicionarCarta(GameObject a)
     {
         mao.Add(a);
@@ -72,6 +85,6 @@ public class Mao : MonoBehaviour
         SetCarta(0);
         SetCarta(0);
         SetCarta(0);
-        SetAnguloZ(12);
+        SetAnguloTeste(12);
     }
 }
